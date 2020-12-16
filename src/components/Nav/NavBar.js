9bb7f309ebdb0017b921f13feaa606e1c7a5f722
LukeZvada <zvadamusic@gmail.com>
2020-12-15 23:03:59 -0500

    added new nav bar with media query that changes to hamburger menu at designated size

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -3,26 +3,27 @@ import "./NavBar.css"
 
 export const NavBar = () => {
     return (
-        <section className="navbar">
-            <div className="navbarTitle">
-                <Link className="navbar__titleLink circle" to="/dashboard"><div className="circle"></div><span className="pilotText">Pilot</span> <span className="logText">Log</span></Link>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/dashboard">Dashboard <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/lognewflight">Log New Flight</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/metars">Metars</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/airportinformation">Airport Information</a>
+                    </li>
+                </ul>
             </div>
-            <div className="navbar__item">
-                <Link className="navbar__link" to="/dashboard"><div className ="icon">Dashboard</div></Link>
-            </div>
-            <div className="navbar__item">
-                <Link className="navbar__link" to="/lognewflight"><div className ="icon">Log New Flight</div></Link>
-            </div>
-            <div className="navbar__item">
-                <Link className="navbar__link" to="/metars"><div className ="icon">METARS</div></Link>
-            </div>
-            <div className="navbar__item">
-                <Link className="navbar__link" to="/airportinformation"><div className ="icon">Airport Information</div></Link>
-            </div>
-            <div className="navbar__item">
-                <Link className="navbar__link" to="/logout"><div className ="icon">Logout</div></Link>
-            </div>
-            
-        </section>
+        </nav>
     )
 }
