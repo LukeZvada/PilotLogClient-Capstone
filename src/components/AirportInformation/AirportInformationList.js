79f8fd46df24f8c27a9f6fec5907f9b282574f4d
LukeZvada <zvadamusic@gmail.com>
2020-12-28 13:23:08 -0600

    added new placeholder text fo UX and added search bar title

--- src/components/AirportInformation/AirportInformationList.js
+++ src/components/AirportInformation/AirportInformationList.js
@@ -15,7 +15,8 @@ export const AirportInfoList = (props) => {
         <>
             <article className="airport_search">
                 <div class="active-cyan-3 active-cyan-4 mb-4">
-                    <input onChange={event => setAirport(event.target.value)} class="form-control" type="text" placeholder="Search ICAO" aria-label="Search"></input>
+                    <div className="search_title">Search ICAO: </div>
+                    <input onChange={event => setAirport(event.target.value)} class="form-control" type="text" placeholder="ex: KBNA" aria-label="Search"></input>
                 </div>
             </article>
 
